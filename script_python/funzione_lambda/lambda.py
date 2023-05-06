cfd0e28db4e3faaf81309ad8ea7eb0da19bee3fb
Francesco Zompanti <65298163+JustCh3cco@users.noreply.github.com>
2023-05-06 12:28:25 +0200

    Funzione lambda da implementare

--- script_python/funzione_lambda/lambda.py
+++ script_python/funzione_lambda/lambda.py
@@ -1,11 +1,9 @@
 import math
 
-stringa_funzione = input("Inserisci la tua funzione matematica: ")
+stringa_funzione = input("Inserisci una funzione: ") #prendi in input la funzione di cui vuoi calcolarne un x valore
 
-# funzione lambda come parametro di eval()
-funzione_lambda = eval("lambda x: " + stringa_funzione)
+funzione_lambda = eval("lambda x: " + stringa_funzione) #conversione della stringa in una funzione lambda tramite la funzione eval
 
-#funzione lambda come input
-valore_input = float(input("Inserisci il valore di input: "))
-risultato = funzione_lambda(valore_input)
-print("Il risultato è:", risultato)
+valore_input = float(input("Inserisci un valore per calcolare la funzione: ")) #funzione lambda come input
+risultato = funzione_lambda(valore_input) #assegna a risultato la funzione lambda calcolata con la variabile "valore_input"
+print("Il risultato è:", risultato) #stampa a schermo il risultato
