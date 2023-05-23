a904ddf24d71828005db3fb40a825ad945ce3578
Francesco Zompanti <65298163+JustCh3cco@users.noreply.github.com>
2023-05-23 12:11:57 +0200

    Delete script_python/funzione_lambda directory

--- script_python/funzione_lambda/lambda.py
+++
@@ -1,9 +0,0 @@
-import math
-
-stringa_funzione = input("Inserisci una funzione: ") #prendi in input la funzione di cui vuoi calcolarne un x valore
-
-funzione_lambda = eval("lambda x: " + stringa_funzione) #conversione della stringa in una funzione lambda tramite la funzione eval
-
-valore_input = float(input("Inserisci un valore per calcolare la funzione: ")) #funzione lambda come input
-risultato = funzione_lambda(valore_input) #assegna a risultato la funzione lambda calcolata con la variabile "valore_input"
-print("Il risultato è:", risultato) #stampa a schermo il risultato
